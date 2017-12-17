6541afdcc13c1991496891a28e7783005a0bb90d
wuxiaolian <2501038982@qq.com>
2017-12-17 10:18:13 +0800

    Update model.py

--- model.py
+++ model.py
@@ -8,18 +8,39 @@ from tensorflow.python.client import timeline
 import utils
 import os,sys
 slim=tf.contrib.slim
-from STN import spatial_transformer_network as stn
-from densenet import *
-from TPS import ThinPlateSpline2 as tps
-istraining=True
-
-
+#import STN as stn
+from TPS import ThinPlateSpline2 as stn
 FLAGS=utils.FLAGS
+from densenet import *
 #26*2 + 10 digit + blank + space
 num_classes=utils.num_classes
-
+max_timesteps=0
 num_features=utils.num_features
 
+def batch_norm(inputs, is_training, is_conv_out=True, decay=0.999):
+    # is_training presents the net training or testing;is_conv_out presents the layer whether convolutioon
+    # is_training表示网络是否训练，is_conv_out表示该层是不是卷积层
+    scale = tf.Variable(tf.ones([inputs.get_shape()[-1]]))
+    beta = tf.Variable(tf.zeros([inputs.get_shape()[-1]]))
+    pop_mean = tf.Variable(tf.zeros([inputs.get_shape()[-1]]), trainable=False)
+    pop_var = tf.Variable(tf.ones([inputs.get_shape()[-1]]), trainable=False)
+
+    if is_training:
+        if is_conv_out:
+            batch_mean, batch_var = tf.nn.moments(inputs, [0, 1, 2])
+        else:
+            batch_mean, batch_var = tf.nn.moments(inputs, [0])
+
+        train_mean = tf.assign(pop_mean,
+                               pop_mean * decay + batch_mean * (1 - decay))
+        train_var = tf.assign(pop_var,
+                              pop_var * decay + batch_var * (1 - decay))
+        with tf.control_dependencies([train_mean, train_var]):
+            return tf.nn.batch_normalization(inputs,
+                                             batch_mean, batch_var, beta, scale, 0.001)
+    else:
+        return tf.nn.batch_normalization(inputs,
+                                         pop_mean, pop_var, beta, scale, 0.001)
 def stacked_bidirectional_rnn(RNN, num_units, num_layers, inputs, seq_lengths):
     """
     multi layer bidirectional rnn
@@ -30,7 +51,9 @@ def stacked_bidirectional_rnn(RNN, num_units, num_layers, inputs, seq_lengths):
     :param seq_lengths: list or 1-D Tensor, sequence length, a list of sequence lengths, the length of the list is batch_size
     :param batch_size: int
     :return: the output of last layer bidirectional rnn with concatenating
-
+    这里用到几个tf的特性
+    1. tf.variable_scope(None, default_name="bidirectional-rnn")使用default_name
+    的话,tf会自动处理命名冲突
     """
     # TODO: add time_major parameter, and using batch_size = tf.shape(inputs)[0], and more assert
     _inputs = inputs
@@ -38,6 +61,8 @@ def stacked_bidirectional_rnn(RNN, num_units, num_layers, inputs, seq_lengths):
         raise ValueError("the inputs must be 3-dimentional Tensor")
 
     for _ in range(num_layers):
+        # 为什么在这加个variable_scope,被逼的,tf在rnn_cell的__call__中非要搞一个命名空间检查
+        # 恶心的很.如果不在这加的话,会报错的.
         with tf.variable_scope(None, default_name="bidirectional-rnn"):
             rnn_cell_fw = RNN(num_units)
             rnn_cell_bw = RNN(num_units)
@@ -50,57 +75,86 @@ class Graph(object):
     def __init__(self,is_training=True):
         self.graph = tf.Graph()
         with self.graph.as_default():
-            self.inputs = tf.placeholder(tf.float32, [FLAGS.batch_size, utils.image_width, utils.image_height, 1])
-            with tf.variable_scope('STN'):
+            self.inputs = tf.placeholder(tf.float32, [None, utils.image_width, utils.image_height, 1])
+            '''with tf.variable_scope('STN'):
                 #Localisation net
                 conv1_loc = slim.conv2d(self.inputs, 32, [3, 3], scope='conv1_loc')
                 pool1_loc = slim.max_pool2d(conv1_loc, [2, 2], scope='pool1_loc')
                 conv2_loc = slim.conv2d(pool1_loc, 64, [3, 3], scope='conv2_loc')
                 pool2_loc = slim.max_pool2d(conv2_loc, [2, 2], scope='pool2_loc')
                 pool2_loc_flat = slim.flatten(pool2_loc)
-                fc1_loc = tf.layers.dense(pool2_loc,2048,activation=tf.nn.relu,kernel_initializer=tf.zeros_initializer)
-                fc2_loc = tf.layers.dense(fc1_loc,512,activation=tf.nn.relu,kernel_initializer=tf.zeros_initializer)
-                fc3_loc = tf.layers.dense(fc2_loc,6,activation=tf.nn.tanh,kernel_initializer=tf.zeros_initializer)
-                h_trans = stn(self.inputs, fc3_loc, (utils.image_width, utils.image_height))
+                fc1_loc = slim.fully_connected(pool2_loc_flat, 1024, scope='fc1_loc')
+                fc2_loc = slim.fully_connected(fc1_loc, 128, scope='fc2_loc')
+                W = tf.Variable(tf.zeros([128, 20]))
+                b = tf.Variable(initial_value=[-1, -0.2, -0.5, -0.35, 0, -0.5, 0.5, -0.67, 1, -0.8,
+                                               -1, 0.8, -0.5, 0.65, 0, 0.5, 0.5, 0.33, 1, 0.2], dtype=tf.float32)
+                # fc3_loc=tf.layers.dense(fc2_loc,20,activation=tf.nn.tanh,kernel_initializer=tf.zeros_initializer)
+                # fc3_loc = slim.fully_connected(fc2_loc, 8, activation_fn=tf.nn.tanh, scope='fc3_loc')
+                # spatial transformer
+                fc3_loc = tf.nn.tanh(tf.matmul(fc2_loc, W) + b)
+                loc = tf.reshape(fc3_loc, [-1, 10, 2])
+                # spatial transformer
+                s = np.array([[-0.95, -0.95], [-0.5, -0.95], [0, -0.95], [0.5, -0.95], [0.95, -0.95], [-0.95, 0.95], [-0.5, 0.95], [0, 0.95], [0.5, 0.95],
+                              [0.95,0.95]] * 256)
+                s = tf.constant(s.reshape([256, 10, 2]), dtype=tf.float32)
+                self.h_trans = stn(self.inputs, s, loc, (utils.image_width, utils.image_height))'''
             if FLAGS.Use_CRNN:
                 with tf.variable_scope('CNN'):
-                    net = slim.conv2d(h_trans, 64, [3, 3], scope='conv1')
+                    net = slim.conv2d(self.inputs, 64, [3, 3], scope='conv1')
                     net = slim.max_pool2d(net, [2, 2], scope='pool1')
                     net = slim.conv2d(net, 128, [3, 3], scope='conv2')
                     net = slim.max_pool2d(net, [2, 2], scope='pool2')
-                    net = slim.conv2d(net, 256, [3, 3],activation_fn=None, scope='conv3')
+                    net = slim.conv2d(net, 256, [3, 3], activation_fn=None, scope='conv3')
                     net = tf.layers.batch_normalization(net, training=is_training)
-                    net=tf.nn.relu(net)
+                    net = tf.nn.relu(net)
                     net = slim.conv2d(net, 256, [3, 3], scope='conv4')
                     net = slim.max_pool2d(net, [2, 2], [1, 2], scope='pool3')
-                    net = slim.conv2d(net, 512, [3, 3],activation_fn=None, scope='conv5')
+                    net = slim.conv2d(net, 512, [3, 3], activation_fn=None, scope='conv5')
                     net = tf.layers.batch_normalization(net, training=is_training)
-                    net=tf.nn.relu(net)
+                    net = tf.nn.relu(net)
                     net = slim.conv2d(net, 512, [3, 3], scope='conv6')
                     net = slim.max_pool2d(net, [2, 2], [1, 2], scope='pool4')
-                    net = slim.conv2d(net, 512, [2, 2], padding='VALID', activation_fn=None,scope='conv7')
+                    net = slim.conv2d(net, 512, [2, 2], padding='VALID', activation_fn=None, scope='conv7')
                     net = tf.layers.batch_normalization(net, training=is_training)
-                    net=tf.nn.relu(net)
-                    self.cnn_time =net.get_shape().as_list()[1]
+                    net = tf.nn.relu(net)
+                    self.cnn_time = net.get_shape().as_list()[1]
+                    self.num_feauture=512
             else:
                 with tf.variable_scope('Dense_CNN'):
                     nb_filter = 64
-                    net=tf.layers.conv2d(h_trans,nb_filter, 5,(2,2), "SAME", use_bias=False)
-                    net,nb_filter=dense_block(net,8,8,nb_filter,is_training)
-                    net,nb_filter=transition_block(net,128,is_training,pooltype=2)
-                    net,nb_filter=dense_block(net,8,8,nb_filter,is_training)
-                    net,nb_filter=transition_block(net,128,istraining,pooltype=3)
-                    net,nb_filter=dense_block(net,8,8,nb_filter,is_training)
-                    net,nb_filter=transition_block(net,128,is_training,pooltype=3)
-                    net=tf.layers.conv2d(net,nb_filter, 3,(1,2), "SAME", use_bias=False)
-                    self.cnn_time =net.get_shape().as_list()[1]
+                    net = tf.layers.conv2d(self.inputs, nb_filter, 5, (2, 2), "SAME", use_bias=False)
+                    net, nb_filter = dense_block(net, 8, 8, nb_filter, is_training)
+                    net, nb_filter = transition_block(net, 128, is_training, pooltype=2)
+                    net, nb_filter = dense_block(net, 8, 8, nb_filter, is_training)
+                    net, nb_filter = transition_block(net, 128, is_training, pooltype=3)
+                    net, nb_filter = dense_block(net, 8, 8, nb_filter, is_training)
+                    #net, nb_filter = transition_block(net, 128, is_training, pooltype=3)
+                    print(net)
+                    #net = tf.layers.conv2d(net, nb_filter, 3, (1, 2), "SAME", use_bias=True)
+                    self.cnn_time = net.get_shape().as_list()[1]
+                    self.num_feauture=4*192
+
+
 
             temp_inputs = net
             with tf.variable_scope('BLSTM'):
                 self.labels = tf.sparse_placeholder(tf.int32)
-                self.lstm_inputs = tf.reshape(temp_inputs, [-1, self.cnn_time, 512])
+                self.lstm_inputs = tf.reshape(temp_inputs, [-1, self.cnn_time, self.num_feauture])
                 # 1d array of size [batch_size]
                 self.seq_len = tf.placeholder(tf.int32, [None])
+                # Defining the cell
+                # Can be:
+                #   tf.nn.rnn_cell.RNNCell
+                #   tf.nn.rnn_cell.GRUCell
+                # cell = tf.contrib.rnn.LSTMCell(FLAGS.num_hidden, state_is_tuple=True)
+                # cell = tf.contrib.rnn.DropoutWrapper(cell = cell,output_keep_prob=0.8)
+                #
+                # cell1 = tf.contrib.rnn.LSTMCell(FLAGS.num_hidden, state_is_tuple=True)
+                # cell1 = tf.contrib.rnn.DropoutWrapper(cell = cell1,output_keep_prob=0.8)
+                # Stacking rnn cells
+                # stack = tf.contrib.rnn.MultiRNNCell([cell,cell1] , state_is_tuple=True)
+
+                # stack = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.LSTMCell(FLAGS.num_hidden,state_is_tuple=True) for _ in range(FLAGS.num_layers)] , state_is_tuple=True)
                 outputs = stacked_bidirectional_rnn(tf.contrib.rnn.LSTMCell, FLAGS.num_hidden, 2, self.lstm_inputs,self.seq_len)
             # The second output is the last state and we will no use that
             # outputs, _ = tf.nn.dynamic_rnn(stack, self.lstm_inputs, self.seq_len, dtype=tf.float32)
@@ -126,8 +180,7 @@ class Graph(object):
 
             self.optimizer = tf.train.MomentumOptimizer(learning_rate=self.learning_rate, momentum=FLAGS.momentum, use_nesterov=True).minimize(self.cost,
                                                                                                             global_step=self.global_step)
-            # self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.initial_learning_rate,
-            # beta1=FLAGS.beta1,beta2=FLAGS.beta2).minimize(self.loss,global_step=self.global_step)
+            #self.optimizer = tf.train.AdadeltaOptimizer(learning_rate=FLAGS.initial_learning_rate).minimize(self.loss,global_step=self.global_step)
 
             # Option 2: tf.contrib.ctc.ctc_beam_search_decoder
             # (it's slower but you'll get better results)
@@ -140,3 +193,4 @@ class Graph(object):
             tf.summary.scalar('cost', self.cost)
             # tf.summary.scalar('lerr',self.lerr)
             self.merged_summay = tf.summary.merge_all()
+#G=Graph()
